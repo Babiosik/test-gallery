bc6282ef2abc26c422da66370e71fd655f435008
Babiosik <babiosik.satan@gmail.com>
2021-01-04 16:23:08 +0200

    Change CardGallery to StatelessWidget

--- lib/cardGalleryWidget.dart
+++ lib/cardGalleryWidget.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:test_task/unsplash.dart';
 
-class CardGallery extends StatefulWidget {
+class CardGallery extends StatelessWidget {
   final UnsplashPhotoItem unsplashPhotoItem;
 
   const CardGallery({Key key, this.unsplashPhotoItem}) : super(key: key);
 
   @override
-  _CardGalleryState createState() => _CardGalleryState();
-}
-
-class _CardGalleryState extends State<CardGallery> {
-  @override
   Widget build(BuildContext context) {
     return new FlatButton(
       onPressed: () {
-        Navigator.pushNamed(context, "/image", arguments: widget.unsplashPhotoItem);
+        //Open image
+        Navigator.pushNamed(context, "/image", arguments: unsplashPhotoItem);
       },
       padding: EdgeInsets.all(4),
       child: Card(
@@ -25,12 +21,12 @@ class _CardGalleryState extends State<CardGallery> {
             color: ThemeData.dark().primaryColor,
             alignment: Alignment.center,
             child: Text(
-              widget.unsplashPhotoItem.author,
+              unsplashPhotoItem.author,
               style: TextStyle(color: Colors.blue[700]),
             ),
           ),
           child: Image.network(
-            widget.unsplashPhotoItem.image.thumb,
+            unsplashPhotoItem.image.thumb,
             fit: BoxFit.cover,
             errorBuilder: (context, exception, stackTrace) {
               return Icon(Icons.error_outline);
